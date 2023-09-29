2c04f76c927bc3edf4dac8cca6b123e67faf6352
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-09-29 18:54:58 +0900

    allcocktail을 볼 수 있는 레이아웃,자바추가

---
+++ app/src/main/java/com/example/wfc/AllcocktailActivity.java
@@ -0,0 +1,18 @@
+package com.example.wfc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AllcocktailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allcocktail);
+
+
+    }
+}
