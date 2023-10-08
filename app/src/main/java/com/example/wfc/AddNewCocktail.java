a34ef3be1c207425e80ca2a759ab4c634839db1e
Ahnhyojun <hyojunahn111@naver.com>
2023-10-09 02:37:40 +0900

    setDocument를 수정함으로써 db에 계속 겹쳐져서 저장되는 상황을 앞전의 숫자를 불러옴으로 개개인으로 저장되게 수정

--- app/src/main/java/com/example/wfc/AddNewCocktail.java
+++ app/src/main/java/com/example/wfc/AddNewCocktail.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import com.example.wfc.databinding.ActivityAddNewCocktailBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class AddNewCocktail extends AppCompatActivity {
     private ActivityAddNewCocktailBinding binding;
@@ -36,12 +37,35 @@ public class AddNewCocktail extends AppCompatActivity {
         });
     }
 
-    private void setDocument(FirebaseData data) {
+/*    private void setDocument(FirebaseData data) {
         FirebaseFirestore.getInstance()
                 .collection("cocktail")
                 .document(String.valueOf(data.getCocktailNum()))
                 .set(data)
                 .addOnSuccessListener(aVoid -> binding.textResult.setText("success!"))
                 .addOnFailureListener(e -> binding.textResult.setText("fail!"));
+    }
+}*/
+
+    private void setDocument(FirebaseData data) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("cocktails")
+                .orderBy("cocktailNum", Query.Direction.DESCENDING)
+                .limit(1)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                        FirebaseData lastCocktail = task.getResult().getDocuments().get(0).toObject(FirebaseData.class);
+                        data.setCocktailNum(lastCocktail.getCocktailNum() + 1);
+                    } else {
+                        data.setCocktailNum(1);
+                    }
+
+                    db.collection("cocktails")
+                            .document(String.valueOf(data.getCocktailNum()))
+                            .set(data)
+                            .addOnSuccessListener(aVoid -> binding.textResult.setText("success!"))
+                            .addOnFailureListener(e -> binding.textResult.setText("fail!"));
+                });
     }
 }
