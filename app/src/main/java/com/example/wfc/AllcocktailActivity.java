ff121de2afa24a9ab917ea22d3f30e19fa52708d
Ahnhyojun <hyojunahn111@naver.com>
2023-10-10 01:51:48 +0900

    검색창이 아닌 다른 화면 클릭 시 키보드가 내려가게 설정

--- app/src/main/java/com/example/wfc/AllcocktailActivity.java
+++ app/src/main/java/com/example/wfc/AllcocktailActivity.java
@@ -60,12 +60,12 @@ public class AllcocktailActivity extends AppCompatActivity {
 
         EditText editTextSearch = findViewById(R.id.editTextSearch);
 
-/*        View mainLayout = findViewById(R.id.mainLayout);  // 이 뷰는 activity_allcocktail.xml 파일에서 가장 바깥쪽에 있는 레이아웃의 id여야 합니다.
+        View mainLayout = findViewById(R.id.mainLayout);  // 이 뷰는 activity_allcocktail.xml 파일에서 가장 바깥쪽에 있는 레이아웃의 id여야 합니다.
         mainLayout.setOnTouchListener((v, event) -> {
-            *//*editTextSearch.setText("");  // 검색창 초기화*//* //이 코드드 사용 시 검색창이 아닌 화면을 레이아웃을 눌렀을 때 검색창이 초기화 되고 키보드가 내려감
+            editTextSearch.setText("");  // 검색창 초기화 //이 코드드 사용 시 검색창이 아닌 화면을 레이아웃을 눌렀을 때 검색창이 초기화 되고 키보드가 내려감
             hideKeyboard(v);
             return true;
-        });*/
+        });
 
         editTextSearch.addTextChangedListener(new TextWatcher() {
             @Override
