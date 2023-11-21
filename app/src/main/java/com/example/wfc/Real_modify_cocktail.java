9cb76f2d337383494fdcaad4646bb7722d113b77
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2023-11-21 09:19:08 +0900

    업데이트 성공 시에 토스메시지로 수정이 됐다고 알려주고, 메인창으로 넘어가게 만듬

--- app/src/main/java/com/example/wfc/Real_modify_cocktail.java
+++ app/src/main/java/com/example/wfc/Real_modify_cocktail.java
@@ -3,22 +3,32 @@ package com.example.wfc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Real_modify_cocktail extends AppCompatActivity {
 
     private EditText editTextCocktailName, editTextCockSimpleExplan, editTextTechniques, editTextGlassName, editTextGarnish, editTextRecipe;
     private Button buttonModify;
+/*    private static final int REQUEST_CODE_IMAGE_PICK = 1;
+    private ImageView imageViewCocktail;
+    private String documentId;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +89,13 @@ public class Real_modify_cocktail extends AppCompatActivity {
                             @Override
                             public void onSuccess(Void aVoid) {
                                 Log.d("Firestore", "DocumentSnapshot successfully updated!");
-                                // 업데이트 완료 후 수행할 작업들...
+
+                                Toast.makeText(Real_modify_cocktail.this, "수정이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+
+                                Intent logoutIntent = new Intent(Real_modify_cocktail.this, LogoutActivity.class);
+                                startActivity(logoutIntent);
+
+                                finish();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                             @Override
@@ -89,5 +105,67 @@ public class Real_modify_cocktail extends AppCompatActivity {
                         });
             }
         });
+
+       /* imageViewCocktail = findViewById(R.id.modifyaddbt);
+
+        imageViewCocktail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_IMAGE_PICK);
+            }
+
+
+        });*/
+
+
     }
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_IMAGE_PICK && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            imageViewCocktail.setImageURI(selectedImageUri);  // 이미지뷰에 선택한 이미지를 보여줍니다.
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            StorageReference cocktailImagesRef = storageRef.child("cocktail_images/" + documentId);
+
+            UploadTask uploadTask = cocktailImagesRef.putFile(selectedImageUri);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            cocktailImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    // 업로드한 이미지의 URL을 Firestore에 저장합니다.
+                                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                    db.collection("cocktails").document(documentId)
+                                            .update("imageUrl", uri.toString())
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d("Firestore", "Image URL successfully updated!");
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w("Firestore", "Error updating image URL", e);
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("FirebaseStorage", "Error uploading image", e);
+                        }
+                    });
+        }
+    }
+*/
 }
