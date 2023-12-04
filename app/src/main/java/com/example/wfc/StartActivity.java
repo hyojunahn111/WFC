af56175626acca6dada0d72c44148f0205e3a05e
Ahnhyojun <hyojunahn111@naver.com>
2023-12-04 20:49:19 +0900

    스타팅 엑티비티를 클릭후가 아닌 3초후에 메인화면으로 이동되게 변경

--- app/src/main/java/com/example/wfc/StartActivity.java
+++ app/src/main/java/com/example/wfc/StartActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,9 +22,20 @@ public class StartActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        stbt = (Button) findViewById(R.id.appstartbt);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (auth.getCurrentUser() != null) {
+                    startActivity(new Intent(StartActivity.this, LogoutActivity.class));
+                } else {
+                    startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                }
+                finish(); // 현재 액티비티를 종료합니다. 이 부분이 없으면, '뒤로 가기' 버튼을 눌렀을 때 이 화면으로 돌아오게 됩니다.
+            }
 
-        stbt.setOnClickListener(new View.OnClickListener() {
+            //stbt = (Button) findViewById(R.id.appstartbt);
+
+        /*stbt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(auth.getCurrentUser() != null){
@@ -32,6 +44,7 @@ public class StartActivity extends AppCompatActivity {
                     startActivity(new Intent(StartActivity.this, LoginActivity.class));
                 }
             }
-        });
+        });*/
+        }, 3000);
     }
 }
